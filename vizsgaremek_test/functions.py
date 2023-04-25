60103d822e776f1af3665d876de750466216c345
Gyulai Csanád <csagyu@gmail.com>
2023-04-25 09:03:08 +0200

    automata tesztek

--- vizsgaremek_test/functions.py
+++ vizsgaremek_test/functions.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import time
-import data import *
+from data import *
 
 
 def login(browser):
